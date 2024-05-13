52afbb88a06a4e637f1081520e51360effcd74a5
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-13 17:17:39 +0200

    creata istruzione condizionale per vedere chi ha vinto , chi ha perso o pareggiato

--- gioco dei dadi/script/script.js
+++ gioco dei dadi/script/script.js
@@ -4,3 +4,11 @@ const pcNumber=Math.floor(Math.random() * (1, 7));
 
 console.log(userNumber)
 console.log(pcNumber)
+
+if(pcNumber > userNumber){
+    console.log("hai perso")
+}else if (userNumber > pcNumber){
+    console.log("hai vinto")
+}else{
+   console.log("pareggio")
+}
