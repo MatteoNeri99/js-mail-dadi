4e96fcb882ba5d427c86f81e378c5df51f63a81a
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-13 17:36:40 +0200

    creata istruzione condizionale che controlli se l'email dell'utente sia presente nell'array ed stampi risposta in console sia se è presente sia se non lo è

--- mail/script/script.js
+++ mail/script/script.js
@@ -1,2 +1,9 @@
 const email=["ioentro@gmail.com", "ioposso@gmail.com", "ancheio@gmail.com","senonentroiononentranessuno@gmail.com"]
 
+const emailUtente=(prompt("scrivi la tua email"))
+
+if(email.includes(emailUtente)){
+    console.log("la tua email è presente nella lista")
+}else{
+    console.log(" la tua email non è presente nella lista")
+}
