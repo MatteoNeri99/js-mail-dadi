83dda65a0035ee1e5ca74816fd373b69bb95646e
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-13 17:57:17 +0200

    aggiunta istruzione condizionale che aggiunge i numeri all'array solo se sono pari
    aggiunto console.log che stampa array alla fine del ciclo

--- saltare a piè dispari/script/script.js
+++ saltare a piè dispari/script/script.js
@@ -2,5 +2,12 @@ const numeriPari=[];
 
 for (let i=0; i<6; i++){
     let userNumber=(prompt("inserisci un numero"));
+
+    if(userNumber %2==0){
+        numeriPari.push(userNumber)
+    }
+
     
 }
+
+console.log(numeriPari)
